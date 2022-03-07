3083ebed3cb8671a05a88f50a3c81aa6d9547c4a
blue <17608488344@163.com>
2022-03-07 15:24:27 +0800

    .

--- blog/src/main/java/com/shiyi/service/impl/LoginServiceImpl.java
+++ blog/src/main/java/com/shiyi/service/impl/LoginServiceImpl.java
@@ -1,27 +1,13 @@
 package com.shiyi.service.impl;
 
 import com.google.code.kaptcha.Producer;
-import com.shiyi.common.RedisConstants;
-import com.shiyi.mapper.RoleMapper;
-import com.shiyi.mapper.UserMapper;
-import com.shiyi.mapper.UserRoleMapper;
 import com.shiyi.service.LoginService;
-import com.shiyi.utils.Base64;
-import com.shiyi.utils.RedisCache;
-import com.shiyi.utils.UUIDUtil;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.util.FastByteArrayOutputStream;
-
 import javax.annotation.Resource;
-import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author blue
@@ -31,28 +17,13 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class LoginServiceImpl implements LoginService {
 
-    @Autowired
-    private UserMapper userMapper;
-
-    @Autowired
-    private RedisCache redisCache;
-
     @Resource(name = "captchaProducerMath")
     private Producer captchaProducerMath;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private RoleMapper roleMapper;
-
-    @Autowired
-    private UserRoleMapper userRoleMapper;
 
     @Override
     public Map<String, String> getCode(HttpServletResponse response) throws IOException {
         Map<String, String> result = new HashMap<String,String>();
-        // 生成验证码的UUID
+      /*  // 生成验证码的UUID
         String uuid = UUIDUtil.getUuid();
         String capStr = null;
         String code = null;
@@ -67,7 +38,7 @@ public class LoginServiceImpl implements LoginService {
         FastByteArrayOutputStream os = new FastByteArrayOutputStream();
         ImageIO.write(image, "jpg", os);
         result.put("uuid",uuid);
-        result.put("img", Base64.encode(os.toByteArray()));
+        result.put("img", Base64.encode(os.toByteArray()));*/
         return result;
     }
 }
