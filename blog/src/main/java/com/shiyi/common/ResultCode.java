effada9ff115e0e14f3626b264975359b30b52ec
quequnlong <qunlong520>
2022-03-31 09:17:27 +0800

    all

--- blog/src/main/java/com/shiyi/common/ResultCode.java
+++ blog/src/main/java/com/shiyi/common/ResultCode.java
@@ -6,7 +6,7 @@ package com.shiyi.common;
  *
  * @description :
  * @author : by blue
- * @date : 2019/8/22 11:09
+ * @date : 2021/8/22 11:09
  */
 public enum ResultCode {
     //成功
@@ -23,14 +23,11 @@ public enum ResultCode {
     WEIBO_LOGIN_ERROR(53002, "微博登录错误"),
 
     GITEE_LOGIN_ERROR(53002, "gitee登录错误"),
-    //服务器内部错误
-    INTERNAL_SERVER_ERROR( 500, "服务器内部错误" ),
 
     // 系统级别错误码
     ERROR(-1, "操作异常"),
     ERROR_DEFAULT(500,"系统繁忙，请稍后重试"),
     NOT_LOGIN(401, "请先登录!"),
-    ILLegal(-6, "非法操作!"),
     NO_PERMISSION(-7,"无权限"),
     ERROR_PASSWORD(-8,"用户帐号或者密码错误!"),
     DISABLE_ACCOUNT(-9,"帐号已被禁用!"),
@@ -39,8 +36,7 @@ public enum ResultCode {
     // 服务层面
     ERROR_EXCEPTION_MOBILE_CODE(10003,"验证码不正确或已过期，请重新输入"),
     ERROR_USER_NOT_EXIST(10009, "用户不存在"),
-    ERROR_MUST_REGISTER(10017,"请先注册帐号!"),
-    EXIST_USER(10018,"用户名已存在,请重试!");
+    ERROR_MUST_REGISTER(10017,"请先注册帐号!");
 
     public int code;
     public String desc;
